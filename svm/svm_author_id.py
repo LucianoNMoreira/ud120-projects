f260a36e8302f3b9298e311c7b6119ef9c6863b0
LucianoNMoreira <lucianonobremoreira@gmail.com>
2016-12-19 09:05:30 -0300

    2 - SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,42 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+# clf = svm.SVC(kernel="linear")
+# clf = svm.LinearSVC()
+clf = svm.SVC(C=10000.0, kernel="rbf")
+
+#Diminuir o dataset para 1%
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+predicts = clf.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
+
+print "Prediction 10 = " + str(predicts[10])
+print "Prediction 26 = " + str(predicts[26])
+print "Prediction 50 = " + str(predicts[50])
+
+chris = 0
+for i in range(len(predicts)):
+    if(predicts[i] == 1):
+        chris+= 1
+
+print "Chris e-mails: " + str(chris)
+
+# print predicts
+t0 = time()
+print "Accuracy: %f" % accuracy_score(labels_test, predicts)
+print "accuracy time:", round(time()-t0, 3), "s"
+
+
 #########################################################
 
 
